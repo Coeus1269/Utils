82a6cc321da6ed6a072e042e3016e85e333a05d1
willgivan <willgivan@dcaservices.com>
2019-01-15 10:02:53 -0600

    Added Versions - Display the versions of various packages

---
+++ src/Versions/Versions.java
@@ -0,0 +1,25 @@
+package Versions;
+
+public class Versions 
+{// Display the versions of various packages
+
+	public static void main(String[] args) 
+	{
+	// hibernate
+	try {
+        String hibernateVersion = org.hibernate.annotations.common.Version.VERSION;
+        System.out.println("Hibernate common Version: "+ hibernateVersion);
+        
+        hibernateVersion = org.hibernate.validator.Version.VERSION;
+        System.out.println("Hibernate validator Version: "+ hibernateVersion);
+        
+        hibernateVersion = org.hibernate.ejb.Version.VERSION;
+        System.out.println("Hibernate ejb Version: "+ hibernateVersion);
+
+    	} 
+	catch (Exception e) 
+		{ e.printStackTrace(); }
+
+	}
+}
+
